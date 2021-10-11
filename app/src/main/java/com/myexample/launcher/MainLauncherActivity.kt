23bd414e7dd234283c4a65215013c46e4395a0f4
Dima <di-doctor@yandex.ru>
2021-10-11 21:31:51 +0300

    start

---
+++ app/src/main/java/com/myexample/launcher/MainLauncherActivity.kt
@@ -0,0 +1,49 @@
+package com.myexample.launcher
+
+import android.content.Intent
+import android.content.pm.ResolveInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainLauncherActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_launcher)
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+        Log.d("TAG","Found ${activities.size} activites")
+    }
+
+    private  class ActivityHolder(itemView : View):RecyclerView.ViewHolder(itemView){
+        private val nameTextView = itemView as TextView
+        private  lateinit var resolveInfo: ResolveInfo
+        fun bindActivity(resolveInfo: ResolveInfo){
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+        }
+    }
+    private  class ActivityAdapter
+}
